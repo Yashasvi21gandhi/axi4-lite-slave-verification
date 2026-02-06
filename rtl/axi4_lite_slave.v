4bb2b87f2df651bd8ecef42f67730c27d672a705
Yashasvi_gandhi <138252245+Yashasvi21gandhi@users.noreply.github.com>
2026-02-07 03:01:50 +0530

    Update axi4_lite_slave.v

--- rtl/axi4_lite_slave.v
+++ rtl/axi4_lite_slave.v
@@ -1,41 +1,33 @@
 module axi4_lite_slave (
-    input  wire        ACLK,
-    input  wire        ARESETn,
-
+    input  wire ACLK,
+    input  wire ARESETn,
     // Write Address Channel
     input  wire [31:0] AWADDR,
-    input  wire        AWVALID,
-    output reg         AWREADY,
-
+    input  wire AWVALID,
+    output reg AWREADY,
     // Write Data Channel
     input  wire [31:0] WDATA,
     input  wire [3:0]  WSTRB,
-    input  wire        WVALID,
-    output reg         WREADY,
-
+    input  wire WVALID,
+    output reg WREADY,
     // Write Response Channel
     output reg  [1:0]  BRESP,
-    output reg         BVALID,
-    input  wire        BREADY,
-
+    output reg BVALID,
+    input  wire BREADY,
     // Read Address Channel
     input  wire [31:0] ARADDR,
-    input  wire        ARVALID,
-    output reg         ARREADY,
-
+    input  wire ARVALID,
+    output reg ARREADY,
     // Read Data Channel
-    output reg [31:0]  RDATA,
-    output reg [1:0]   RRESP,
-    output reg         RVALID,
-    input  wire        RREADY
+    output reg [31:0] RDATA,
+    output reg [1:0] RRESP,
+    output reg RVALID,
+    input  wire RREADY
 );
-
     reg [31:0] regfile [0:3];
     wire [1:0] addr_index;
     assign addr_index = AWADDR[3:2];
-
     integer i;
-
     always @(posedge ACLK) begin
         if (!ARESETn) begin
             AWREADY <= 0;
@@ -49,22 +41,17 @@ module axi4_lite_slave (
         end else begin
             AWREADY <= AWVALID;
             WREADY  <= WVALID;
-
             if (AWVALID && WVALID) begin
                 regfile[addr_index] <= WDATA;
                 BVALID <= 1'b1;
             end
-
             if (BVALID && BREADY)
                 BVALID <= 1'b0;
-
             ARREADY <= ARVALID;
-
             if (ARVALID) begin
                 RDATA  <= regfile[ARADDR[3:2]];
                 RVALID <= 1'b1;
             end
-
             if (RVALID && RREADY)
                 RVALID <= 1'b0;
         end
